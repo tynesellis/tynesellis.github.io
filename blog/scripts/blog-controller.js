2f1c5c13c9e7cd0405ed4115bb1455ea7ee504be
Paul Ellis <tynesellis@gmail.com>
2017-10-30 19:45:59 -0500

    search functioning, displaying results, and displaying selected result

--- blog/scripts/blog-controller.js
+++ blog/scripts/blog-controller.js
@@ -9,7 +9,7 @@ const howManyPages = Math.ceil(numberOfBlogs / blogsPerPage);//calculate how man
 let paginationContent = "<ul class='d-flex flex-row' id='paginate'>";//start the ul that will contain the pageinator
 paginationContent += "<a href='#' id='back' class=''>&lt;</a>";//add the back button
 for (var i = 0; i < howManyPages; i++) {//add a numbered button for each page available
-    paginationContent += `<li><a href="#" class="blog-pages page-${i+1}">${i+1}</a></li>`
+    paginationContent += `<li><a href="#" class="blog-pages page-${i + 1}">${i + 1}</a></li>`
 }
 paginationContent += "<a href= '#' id='next' class='page-2'>&gt;</a>";//add next button
 paginationContent += "</ul>";//close it up
@@ -28,8 +28,8 @@ function addBlogz(event) {
     bloggyHTML.innerHTML = "";//clear out whatever is on the page
     const buttonNumber = parseInt(//get the number of the button clicked and Steve's invitation to visit MDN
         Array.from(event.target.classList)//make an array from returned classList
-        .find(buttonClass => {if (buttonClass.startsWith("page-"))return buttonClass})//find any "page-" class
-        .split("-")[1]//extract the number, which is the number of the button
+            .find(buttonClass => { if (buttonClass.startsWith("page-")) return buttonClass })//find any "page-" class
+            .split("-")[1]//extract the number, which is the number of the button
     );
     //-----change class and/or display of back button
     if (buttonNumber === 1) {//if your're on button one, make the back arrow hide
@@ -38,7 +38,7 @@ function addBlogz(event) {
         backHTML.style.display = "inline";// if you're on any button other than one, display back arrow and set page class to previous page
         backHTML.className = `page-${buttonNumber - 1}`
     }
-        //-----change class and/or display of back button
+    //-----change class and/or display of back button
 
     if ((buttonNumber + 1) > howManyPages) {//if your're on the last button, make the next arrow hide
         nextHTML.style.display = "none";
@@ -46,13 +46,13 @@ function addBlogz(event) {
         nextHTML.style.display = "inline";// if you're on any button other than the last one, display next arrow and set page class to next page
         nextHTML.className = `page-${buttonNumber + 1}`
     }
-  //--------------write to page---------------
+    //--------------write to page---------------
     const blogsToDisplay = retrievedBlogDB.slice(//set the blogs you want to display
         (buttonNumber - 1) * blogsPerPage, //buttonNumber, drawn from class name - 1, since it's an array * however many we want -- set above
         buttonNumber * blogsPerPage//stopping point
     )
 
-    blogsToDisplay.forEach(function(currentBlog) {
+    blogsToDisplay.forEach(function (currentBlog) {
         bloggyHTML.innerHTML += `
             <h1>${currentBlog.name.value}</h1>
             <h2>Date: ${currentBlog.published.value}</h2>
@@ -82,31 +82,49 @@ nextHTML.addEventListener("click", addBlogz)
 //--------------End Event Listener--------------------------
 
 //Search blogs============================================
-document.getElementById("searchBlogs").addEventListener("keyup", function(){
-    let searchString = event.target.value;
-    if(searchString.length >= 3){
-        const searchResults = retrievedBlogDB.filter(function(checkedBlog){
-          for (let key in checkedBlog){
-            if (toString(checkedBlog[key].value).includes(searchString)){
-                return checkedBlog; 
-            };  
-          }
+document.getElementById("searchBlogs").addEventListener("keyup", function () {
+    let searchString = event.target.value.toLowerCase();
+    if (searchString.length >= 3) {
+        const searchResults = retrievedBlogDB.filter(function (checkedBlog) {
+            for (let key in checkedBlog) {
+                if (checkedBlog[key].value.toString().toLowerCase().includes(searchString)) {
+                    return checkedBlog;
+                };
+            }
         })
-        console.log(searchResults)  
-        
         printedResults = ``;
-        searchResults.forEach(function(searchObj){
+        searchResults.forEach(function (searchObj) {
             printedResults += `
-                <button class="searchedBlogResults contentBoxes">
-                <h4 id="${searchObj.blogID.value}">${searchObj.name.value}</h4>
+            <article class="searchedBlogResults contentBoxes">  
+                <h4>${searchObj.name.value}</h4>
                 <h5 class="text-muted">Date: ${searchObj.published.value}</h5>
-                <article>${searchObj.text.value.substring(0,175)}...<em>click for more<em></article>
-                </button>
+                <article>${searchObj.text.value.substring(0, 175)}...</article>
+                <button id="${searchObj.blogID.value}" class="contentBoxes">...Click for more</button>
+            </article>
             `
-        }) 
+        })
         bloggyHTML.innerHTML = printedResults;
+        //Search Results Event Listener
+        const searchResultButtons = document.getElementsByClassName("searchedBlogResults");
+        for (let b = 0; b < searchResultButtons.length; b++) {
+            let resultButton = searchResultButtons[b];
+            resultButton.addEventListener("click", function (event) {
+                bloggyHTML.innerHTML = "";//clear out whatever is on the page
+                    retrievedBlogDB.forEach(function(blogObj){
+                        if (blogObj.blogID.value.toString() === event.target.id) {
+                            bloggyHTML.innerHTML += `
+                            <h1>${blogObj.name.value}</h1>
+                            <h2>Date: ${blogObj.published.value}</h2>
+                            <article>${blogObj.text.value}</article>
+                        `
+                        }
+                    })
+            })
+        }
     }
 })
+
+
 
 //run at beginning of page load============================
 addBlogz({
--- blog/scripts/blog-factory.js
+++ blog/scripts/blog-factory.js
@@ -21,7 +21,7 @@ const blogObjects = [
         },
         "text": {
             enumerable: true,
-            value: "With week one in the bag, I can already tell this is going to fly by.  The ice broke quickly on day one, and by day two we were jumping in.  We were introduced (or reintroduced) to the multiverse of objects and functions and objects and git….and objects (they’re everywhere).  I get the feeling it’s only going to accelerate from here, but I’m not discouraged.  I’m glad.  Six months isn’t a lot of time, and I want to get everything I can from this program. There will likely be frustrations, as there already have been…like I said, we dove in.<br><br>  I hope we will all keep a perspective of training and learning, rather than attempt and failure.  I’ve had days when I went the the gym and phoned it in, and I’ve had days where I didn’t hold anything back.  In the first case, I felt fine the next day.  In the second…not so much.  But when I didn’t feel the strain of pushing myself and growing from it, I felt like I had wasted my time.  When I pushed myself, I felt a pride in my work, despite being a little beat up from it.  I hope we can all see and remind each other that the struggle is good and that it’s not a sign of failure but of progress."
+            value: "They were four men living all together yet they were all alone. Fish don't fry in the kitchen and beans don't burn on the grill. Took a whole lotta tryin' just to get up that hill. Well we're movin' on up to the east side to a deluxe apartment in the sky. A man is born he's a man of means. Then along come two they got nothin' but their jeans. In a freak mishap Ranger 3 and its pilot Captain William Buck Rogers are blown out of their trajectory into an orbit which freezes his life support systems and returns Buck Rogers to Earth five-hundred years later. Fleeing from the Cylon tyranny the last Battlestar – Galactica - leads a rag-tag fugitive fleet on a lonely quest - a shining planet known as Earth."
         }
     },
     {
@@ -39,7 +39,7 @@ const blogObjects = [
         },
         "text": {
             enumerable: true,
-            value: "With week one in the bag, I can already tell this is going to fly by.  The ice broke quickly on day one, and by day two we were jumping in.  We were introduced (or reintroduced) to the multiverse of objects and functions and objects and git….and objects (they’re everywhere).  I get the feeling it’s only going to accelerate from here, but I’m not discouraged.  I’m glad.  Six months isn’t a lot of time, and I want to get everything I can from this program. There will likely be frustrations, as there already have been…like I said, we dove in.<br><br>  I hope we will all keep a perspective of training and learning, rather than attempt and failure.  I’ve had days when I went the the gym and phoned it in, and I’ve had days where I didn’t hold anything back.  In the first case, I felt fine the next day.  In the second…not so much.  But when I didn’t feel the strain of pushing myself and growing from it, I felt like I had wasted my time.  When I pushed myself, I felt a pride in my work, despite being a little beat up from it.  I hope we can all see and remind each other that the struggle is good and that it’s not a sign of failure but of progress."
+            value: "Pooping rainbow while flying in a toasted bread costume in space chase ball of string chew foot, and poop on grasses. Why must they do that flop over. Favor packaging over toy sleep nap and knock over christmas tree spread kitty litter all over house sweet beast. Use lap as chair give attitude poop on grasses sleep nap. Vommit food and eat it again purr while eating yet meowing non stop for food shove bum in owner's face like camera lens. Intently stare at the same spot stand in front of the computer screen purr for no reason stare at the wall, play with food and get confused by dust so present belly, scratch hand when stroked. Intrigued by the shower have secret plans. Sweet beast find something else more interesting, or stare at ceiling. Leave dead animals as gifts find something else more interesting. Lick butt. Sleep in the bathroom sink why must they do that. Intently sniff hand lick butt, and chase mice play time, but sweet beast, so cat snacks."
         }
     },
     {
@@ -57,7 +57,7 @@ const blogObjects = [
         },
         "text": {
             enumerable: true,
-            value: "With week one in the bag, I can already tell this is going to fly by.  The ice broke quickly on day one, and by day two we were jumping in.  We were introduced (or reintroduced) to the multiverse of objects and functions and objects and git….and objects (they’re everywhere).  I get the feeling it’s only going to accelerate from here, but I’m not discouraged.  I’m glad.  Six months isn’t a lot of time, and I want to get everything I can from this program. There will likely be frustrations, as there already have been…like I said, we dove in.<br><br>  I hope we will all keep a perspective of training and learning, rather than attempt and failure.  I’ve had days when I went the the gym and phoned it in, and I’ve had days where I didn’t hold anything back.  In the first case, I felt fine the next day.  In the second…not so much.  But when I didn’t feel the strain of pushing myself and growing from it, I felt like I had wasted my time.  When I pushed myself, I felt a pride in my work, despite being a little beat up from it.  I hope we can all see and remind each other that the struggle is good and that it’s not a sign of failure but of progress."
+            value: "These are the voyages of the Starship Enterprise. Its continuing mission, to explore strange new worlds, to seek out new life and new civilizations, to boldly go where no one has gone before. We need to neutralize the homing signal. Each unit has total environmental control, gravity, temperature, atmosphere, light, in a protective field. Sensors show energy readings in your area. We had a forced chamber explosion in the resonator coil. Field strength has increased by 3,000 percent.."
         }
     },
     {
@@ -75,7 +75,7 @@ const blogObjects = [
         },
         "text": {
             enumerable: true,
-            value: "With week one in the bag, I can already tell this is going to fly by.  The ice broke quickly on day one, and by day two we were jumping in.  We were introduced (or reintroduced) to the multiverse of objects and functions and objects and git….and objects (they’re everywhere).  I get the feeling it’s only going to accelerate from here, but I’m not discouraged.  I’m glad.  Six months isn’t a lot of time, and I want to get everything I can from this program. There will likely be frustrations, as there already have been…like I said, we dove in.<br><br>  I hope we will all keep a perspective of training and learning, rather than attempt and failure.  I’ve had days when I went the the gym and phoned it in, and I’ve had days where I didn’t hold anything back.  In the first case, I felt fine the next day.  In the second…not so much.  But when I didn’t feel the strain of pushing myself and growing from it, I felt like I had wasted my time.  When I pushed myself, I felt a pride in my work, despite being a little beat up from it.  I hope we can all see and remind each other that the struggle is good and that it’s not a sign of failure but of progress."
+            value: "Hodor hodor - HODOR hodor, hodor HODOR hodor, hodor hodor, hodor. Hodor hodor?! Hodor. Hodor hodor... Hodor hodor HODOR hodor, hodor HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor hodor HODOR! Hodor hodor HODOR hodor, hodor hodor hodor hodor? Hodor! Hodor hodor, hodor... Hodor hodor hodor. Hodor. Hodor hodor - HODOR hodor, hodor hodor, hodor, hodor hodor. Hodor, hodor. Hodor. Hodor, hodor hodor hodor? Hodor, hodor - hodor, hodor, hodor hodor. Hodor! Hodor hodor, hodor hodor hodor hodor hodor hodor, hodor. Hodor hodor hodor! Hodor. Hodor hodor hodor, hodor. Hodor hodor. ."
         }
     },
     {
@@ -83,9 +83,18 @@ const blogObjects = [
             enumerable: true,
             value: 2,
         },
-        "name": "Weeks Two and Three",
-        "published": "10/22/2017",
-        "text": "The last few weeks were the start of our sprints and group projects. It’s been a goldmine of learning and solidifying practices. The Github flow rhythm took a little getting used to, but, after a few pull and push disasters, we all started to figure out the steps. We also learned the importance of keeping the laptop closed and doing some solid white-boarding. It’s an investment that saves a lot of time down the road. The group dynamic gave an exponential return for what each person brought to the table. It took both what we knew and didn’t know individually, mixed it all together, and sent each of us back out with a new knowledge base equal to the contributions of the entire group. As we struggled through what we didn’t know or what we thought we knew but didn’t, the new concepts sunk into brain and muscle memory. I’m looking forward to the next project."
+        "name": {
+            enumerable: true,
+            value: "Weeks Two and Three",
+        },
+        "published": {
+            enumerable: true,
+            value: "10/22/2017",
+        },
+        "text": {
+            enumerable: true,
+            value: "The last few weeks were the start of our sprints and group projects. It’s been a goldmine of learning and solidifying practices. The Github flow rhythm took a little getting used to, but, after a few pull and push disasters, we all started to figure out the steps. We also learned the importance of keeping the laptop closed and doing some solid white-boarding. It’s an investment that saves a lot of time down the road. The group dynamic gave an exponential return for what each person brought to the table. It took both what we knew and didn’t know individually, mixed it all together, and sent each of us back out with a new knowledge base equal to the contributions of the entire group. As we struggled through what we didn’t know or what we thought we knew but didn’t, the new concepts sunk into brain and muscle memory. I’m looking forward to the next project."
+        }
     },
     {
         "blogID": {
